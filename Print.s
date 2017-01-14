d5b3d2f7dd2007fae72108d0d337227af7ca5fd4
ashkanvafaee <ashkanvafaee@utexas.edu>
2017-01-14 15:59:37 -0600

    Update Print.s

--- Print.s
+++ Print.s
@@ -1,10 +1,8 @@
 ; Print.s
-; Student names: change this to your names or look very silly
-; Last modification date: change this to the last modification date or look very silly
+; Last modification date: 5/5/2016
 ; Runs on LM4F120 or TM4C123
 ; EE319K lab 7 device driver for any LCD
 ;
-; As part of Lab 7, students need to implement these LCD_OutDec and LCD_OutFix
 ; This driver assumes two low-level LCD functions
 ; ST7735_OutChar   outputs a single 8-bit ASCII character
 ; ST7735_OutString outputs a null-terminated string 
@@ -108,9 +106,9 @@ FixFindDigit
 	
 	PUSH {R3, R0}		;push local variable onto stack
 	
-	ADD R11, #1			;increment counter
+	ADD R11, #1		;increment counter
 	
-	CMP R11, #4			;if there are still digits left to be displayed, go back and deal with them
+	CMP R11, #4		;if there are still digits left to be displayed, go back and deal with them
 	BNE FixFindDigit
 	
 	POP {R3, R0}
@@ -139,7 +137,7 @@ EndStars
 	BX   LR
 
 Stars
-	MOV R0, #0x2A					;output "*.***"
+	MOV R0, #0x2A		;output "*.***"
 	BL ST7735_OutChar
 	MOV R0,#0x2E
 	BL ST7735_OutChar
